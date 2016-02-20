342a077880f13781201fba7fd5d97f09af3b4be2
Richard Hayler <top_shed@yahoo.com>
2016-02-20 17:29:35 +0000

    added comments

--- underpressure.py
+++ underpressure.py
@@ -1,15 +1,22 @@
-import logging
+import logging # makes it easy to log data to file
 from sense_hat import SenseHat
+from datetime import datetime
 from time import sleep
-logging.basicConfig(filename='testlog.log', level=logging.DEBUG,
-    format='%(asctime)s %(message)s')
-sh = SenseHat()
-p_old = 0
-b = [0,0,0]
-c = [255,0,0]
-d=  [0,255,0]
+
+# Set up the logfile name based on date/time
+logfile = "pressure-"+str(datetime.now().strftime("%Y%m%d-%H%M"))+".csv"
+# Logging settings and format for csv
+logging.basicConfig(filename=logfile, level=logging.DEBUG,
+    format='%(asctime)s %(message)s',
+    datefmt='%Y-%m-%d, %H:%M:%S,')
+
+sh = SenseHat() #Initialise SenseHAT
+p_old = 0 # previous reading
+b = [0,0,0] # All LEDs off
+c = [255,0,0] # Red
+d=  [0,255,0] # Green
 arrow = []
-up_arrow = [
+up_arrow = [ # Up arrow
     b,b,b,c,c,b,b,b,
     b,b,c,c,c,c,b,b,
     b,c,c,c,c,c,c,b,
@@ -18,7 +25,8 @@ up_arrow = [
     b,b,c,c,c,c,b,b,
     b,b,c,c,c,c,b,b,
     b,b,c,c,c,c,b,b]
-down_arrow = [
+
+down_arrow = [ # Down arrow
     b,b,d,d,d,d,b,b,
     b,b,d,d,d,d,b,b,
     b,b,d,d,d,d,b,b,
@@ -28,22 +36,22 @@ down_arrow = [
     b,b,d,d,d,d,b,b,
     b,b,b,d,d,b,b,b]
 
-while True:
+while True: # main loop
    show_arrow = False
-   p = sh.get_pressure()
-   logging.info(str(p))
-   if p > p_old:
-       col = [255,0,0]
-       if (p - p_old) >= 1:
+   p = sh.get_pressure() # Take pressure reading
+   logging.info(str(p)) # Log value to file
+   if p > p_old: # If pressure has increased...
+       col = [255,0,0] # Set to Red
+       if (p - p_old) >= 1: # If change > 1mB
            show_arrow = True
            arrow = up_arrow
-   else:
-       col = [0,255,0]
+   else: # If pressure has decreased...
+       col = [0,255,0] # set to green
        if (p_old - p) >= 1:
            show_arrow = True
            arrow = down_arrow
    sh.show_message(str(round(p,2)),text_colour=col)
    if show_arrow:
-   	sh.set_pixels(arrow)
-   sleep(0.5)
-   p_old = p
+   	sh.set_pixels(arrow) # Draw arrow on LEDs
+   	sleep(0.5)
+   p_old = p # Set previous value
